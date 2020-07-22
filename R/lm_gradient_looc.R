d4db7453067900a63c34473b374a4e89915f6041
Vincenzo Nardelli <vincnardelli@gmail.com>
2020-07-22 16:52:45 +0200

    add looc

---
+++ R/lm_gradient_looc.R
@@ -0,0 +1,35 @@
+lm_gradient_looc <- function(b, formula, data=NULL, maxit=1000,     # max iteration, not to run forever
+                             tolerance=1e-5, # tolerance parameter
+                             stepsize=1e-4,  # stepsize parameter
+                             fun = "sd",
+                             parallel=FALSE){
+
+  .controls(tolerance, maxit, stepsize)
+
+  if(is.null(data)){
+
+    mod <- model.frame.default(formula)
+    y <- model.response(mod, type="numeric")
+    x <- as.matrix(cbind(1,subset(mod, select = -y )))
+
+  }else{
+
+    response <- as.character(attr(terms(formula), "variables"))[-1][attr(terms(formula), "response")] #[1] is the list call
+    y <- data[,match(response, names(data))]
+    vars <- all.vars(formula)[which(all.vars(formula)!= response)]
+    mt <- match(vars, names(data))
+    x <- model.matrix(~., data[mt])
+  }
+
+  out <- lm_gradient_cv(n.folds=nrow(x),
+                                    b=b,
+                                    formula=formula,
+                                    data=data,
+                                    maxit=maxit,     # max iteration, not to run forever
+                                   tolerance=tolerance, # tolerance parameter
+                                   stepsize=stepsize,  # stepsize parameter
+                                   fun = fun,
+                                   parallel=parallel)
+
+  return(out)
+}
--- dev/test.R
+++ dev/test.R
@@ -47,3 +47,5 @@ bench::mark(
   check = FALSE
 )
 
+lm_gradient_looc(b=b, formula=y~x1+x2, data=data, maxit, tolerance, fun="sd", parallel = FALSE)
+
