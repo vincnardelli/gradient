9c8f75462c87c357e019b8dc7c9cd4432539b84a
Vincenzo Nardelli <vincnardelli@gmail.com>
2020-07-22 11:49:37 +0200

    change in test gd

--- tests/testthat/test-gd.R
+++ tests/testthat/test-gd.R
@@ -9,42 +9,43 @@ stepsize <- 1e-5
 tolerance <- 1e-10
 maxit <- 1
 b <- c(0.1, 0.1, 0.1)
-
+data <- data.frame(y=y, x1=x1, x2=x2)
 
 test_that("gd", {
-  expect_equal(gd(b, y~x1+x2, stepsize, maxit, tolerance)$b,
+  expect_equal(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit, tolerance, stepsize, fun="gd")$b,
                structure(c(0.117811702875545, 0.107945369401251, 0.104008346008317
                ), .Dim = c(3L, 1L)))
 })
 
 test_that("gd tolerance reached", {
-  expect_warning(gd(b, y~x1+x2, stepsize, maxit=1000, tolerance)$b,
+  expect_warning(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit=1000, tolerance, stepsize, fun="gd"),
                "Tolerance reached", fixed=TRUE)
 })
 
-# check error < tolerance
+# check error < x
+# check tolerance
 
 test_that("gd tolerance error", {
-  expect_error(gd(b, y~x1+x2, stepsize, maxit, tolerance="ok"),
+  expect_error(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit, tolerance="ok", stepsize, fun="gd"),
                "Tolerance must be numeric", fixed=TRUE)
 })
 
 test_that("gd step error", {
-  expect_error(gd(b, y~x1+x2, stepsize="ok", maxit, tolerance),
+  expect_error(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit, tolerance, stepsize="ok", fun="gd"),
                "Step size must be numeric", fixed=TRUE)
 })
 
 test_that("gd step error", {
-  expect_error(gd(b, y~x1+x2, stepsize, maxit="ok", tolerance),
+  expect_error(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit="ok", tolerance, stepsize, fun="gd"),
                "Max Iteration (maxit) must be numeric", fixed=TRUE)
 })
 
 test_that("gd beta large", {
-  expect_error(gd(b=c(100000, 100000, 100000), y~x1+x2, stepsize=1000, maxit=50, tolerance)$b,
+  expect_error(lm_gradient(b=c(100000, 100000, 100000), formula=y~x1+x2, data=data, maxit=50, tolerance, stepsize=1000, fun="gd"),
                "Beta too large: decrease the step size!", fixed=TRUE)
 })
 
 test_that("gd class", {
-  expect_equal(class(gd(b, y~x1+x2, stepsize, maxit, tolerance)),
+  expect_equal(class(lm_gradient(b=b, formula=y~x1+x2, data=data, maxit, tolerance, stepsize, fun="gd")),
                "gradient")
 })
